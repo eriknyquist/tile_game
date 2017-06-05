d13c2362edfced1e2a336966b5cb9d147f7a21e5
Erik Nyquist <eknyquist@gmail.com>
2017-06-04 20:17:51 -0700

    Create ctrl_t struct

--- src/defs.h
+++ src/defs.h
@@ -6,6 +6,23 @@
 #include <stdlib.h>
 #include "SDL2/SDL.h"
 
+/* Window dimensions */
 #define SHEIGHT 1080
 #define SWIDTH  1920
+
+/* Map array dimensions */
+#define MAX_X 512
+#define MAX_Y 32
+
+/* Tile size in pixels */
+#define TILE_SIZE (SHEIGHT / MAX_Y)
+
+typedef struct control {
+    uint8_t map[MAX_Y][MAX_X];
+    SDL_Window *win;
+    SDL_Renderer *rend;
+} ctrl_t;
+
+extern ctrl_t ctrl;
+
 #endif
--- src/init.c
+++ src/init.c
@@ -1,16 +1,17 @@
 #include "defs.h"
 
+ctrl_t ctrl;
 SDL_Window *window;
 SDL_Renderer *renderer;
 
 /* Sky blue */
 const uint8_t bgcolor[3] = {102, 204, 255};
 
-void set_bg_color(SDL_Window *win, SDL_Renderer *render)
+void set_bg_color(ctrl_t *ctrl)
 {
-    SDL_SetRenderDrawColor(render, bgcolor[0], bgcolor[1], bgcolor[2], 255);
-    SDL_RenderClear(render);
-    SDL_RenderPresent(render);
+    SDL_SetRenderDrawColor(ctrl->rend, bgcolor[0], bgcolor[1], bgcolor[2], 255);
+    SDL_RenderClear(ctrl->rend);
+    SDL_RenderPresent(ctrl->rend);
 }
 
 void init (char *title)
@@ -21,17 +22,17 @@ void init (char *title)
 		exit(1);
 	}
 
-    window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,
+    ctrl.win = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED, SWIDTH, SHEIGHT, 0);
 
-    if (window == NULL) {
+    if (ctrl.win == NULL) {
         printf("Couldn't create %dx%d window: %s\n", SWIDTH, SHEIGHT, 
             SDL_GetError());
 		exit(1);
 	}
 
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    set_bg_color(window, renderer);
+    ctrl.rend = SDL_CreateRenderer(ctrl.win, -1, 0);
+    set_bg_color(&ctrl);
 }
 
 void cleanup (void)
