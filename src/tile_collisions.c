2e0b725030057132c86e3d9277a7558aaf0684d1
Erik Nyquist <eknyquist@gmail.com>
2017-07-07 09:42:26 -0700

    tile_collisions.c: better comments

--- src/tile_collisions.c
+++ src/tile_collisions.c
@@ -10,11 +10,11 @@ int tile_obstacle_left (ctrl_t *ctrl, int x, int y)
     uint8_t map_sym;
     unsigned int xM, yM;
 
-    /* x/y of tile containing the rect's right edge */
+    /* Convert screen point to offsets for on-screen colliders array */
     xM = SCREEN_TO_XTILE(ctrl, x);
     yM = SCREEN_TO_YTILE(y);
 
-    /* Loop through tiles to the left of point, find the first obstacle */
+    /* Loop through tiles left of point, find the first obstacle */
     for (i = xM - 1; i >= 0; --i) {
         map_sym = ctrl->map.data[yM][ctrl->pos + i];
         if (map_sym > 0 && map_sym != PLAYER_FINISH) {
@@ -22,12 +22,12 @@ int tile_obstacle_left (ctrl_t *ctrl, int x, int y)
         }
     }
 
-    /* No tiles to the right of rect */
+    /* No tiles left of point */
     if (i < 0) {
         return NO_TILES;
     }
 
-    /* Return distance between rect and tile */
+    /* Return distance between point and tile */
     return x - (ctrl->colliders[yM][i].x + TILE_SIZE);
 }
 
@@ -38,23 +38,23 @@ int tile_obstacle_right (ctrl_t *ctrl, int x, int y)
     int i;
     unsigned int xM, yM;
 
-    /* x/y of tile containing the rect's right edge */
+    /* Convert screen point to offsets for on-screen colliders array */
     xM = SCREEN_TO_XTILE(ctrl, x);
     yM = SCREEN_TO_YTILE(y);
 
-    /* Loop through tiles to the right of rect, find the first obstacle */
+    /* Loop through tiles right of point, find the first obstacle */
     for (i = xM + 1; i < XTILES_WIDTH; ++i) {
         if (ctrl->map.data[yM][ctrl->pos + i]) {
             break;
         }
     }
 
-    /* No tiles to the right of rect */
+    /* No tiles right of point */
     if (i == XTILES_WIDTH) {
         return NO_TILES;
     }
 
-    /* Return distance between rect and tile */
+    /* Return distance between point and tile */
     return ctrl->colliders[yM][i].x - x;
 }
 
@@ -65,23 +65,23 @@ int tile_obstacle_up (ctrl_t *ctrl, int x, int y)
     int i;
     unsigned int xM, yM;
 
-    /* x/y of tile containing rect's top edge */
+    /* Convert screen point to offsets for on-screen colliders array */
     xM = SCREEN_TO_XTILE(ctrl, x);
     yM = SCREEN_TO_YTILE(y);
 
-    /* Loop through tiles above rect, find the first obstacle */
+    /* Loop through tiles above point, find the first obstacle */
     for (i = yM - 1; i >= 0; --i) {
         if (ctrl->map.data[i][xM + ctrl->pos]) {
             break;
         }
     }
 
-    /* No tiles above rect */
+    /* No tiles above point */
     if (i < 0) {
         return y - ctrl->colliders[0][xM].y;
     }
 
-    /* Return distance between rect and tile */
+    /* Return distance between point and tile */
     return y - (ctrl->colliders[i][xM].y + TILE_SIZE);
 }
 
@@ -92,7 +92,7 @@ int tile_obstacle_down (ctrl_t *ctrl, int x, int y)
     int i;
     unsigned int xM, yM;
 
-    /* x/y of tile containing the rect's bottom edge */
+    /* Convert screen point to offsets for on-screen colliders array */
     xM = SCREEN_TO_XTILE(ctrl, x);
     yM = SCREEN_TO_YTILE(y);
 
@@ -101,18 +101,18 @@ int tile_obstacle_down (ctrl_t *ctrl, int x, int y)
         return BELOW_MAP;
     }
 
-    /* Loop through tiles below rect, find the first obstacle */
+    /* Loop through tiles below point, find the first obstacle */
     for (i = yM + 1; i < YTILES_HEIGHT; ++i) {
         if (ctrl->map.data[i][xM + ctrl->pos]) {
             break;
         }
     }
 
-    /* No tiles below rect */
+    /* No tiles below point */
     if (i == YTILES_HEIGHT) {
         return NO_TILES;
     }
 
-    /* Return distance between rect and tile */
+    /* Return distance between point and tile */
     return ctrl->colliders[i][xM].y - y;
 }
